ff3f1492629087b9899566dc2515bddb89d96e2a
zhangchen <zhangchen0419@qq.com>
2019-07-02 19:16:20 +0800

    SpringCloud入门(七) 第四章 4.3 hystrix-dashboard的使用

--- server/src/main/java/com/imooc/order/OrderApplication.java
+++ server/src/main/java/com/imooc/order/OrderApplication.java
@@ -1,11 +1,15 @@
 package com.imooc.order;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 /*@SpringBootApplication
@@ -14,8 +18,16 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableFeignClients(basePackages = "com.imooc.product.client")
 @SpringCloudApplication
 @ComponentScan(basePackages = "com.imooc")
+@EnableHystrixDashboard
 public class OrderApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OrderApplication.class, args);
+	}
+
+	@Bean
+	public ServletRegistrationBean hystrixMetricsStreamServlet() {
+		ServletRegistrationBean registration = new ServletRegistrationBean(new HystrixMetricsStreamServlet());
+		registration.addUrlMappings("/actuator/hystrix.stream");
+		return registration;
 	}
 }
