c8f8e07c76ac8fe400091c195e0e25e918fd9b2a
zhangchen <zhangchen0419@qq.com>
2019-07-02 12:55:18 +0800

    SpringCloud入门(七) 第四章 探讨断路器模式

--- server/src/main/java/com/imooc/order/Controller/HystrixController.java
+++ server/src/main/java/com/imooc/order/Controller/HystrixController.java
@@ -4,6 +4,7 @@ import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -21,11 +22,15 @@ import java.util.Arrays;
 public class HystrixController {
 
     @HystrixCommand(commandProperties = {
-            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value="3000")
+            @HystrixProperty(name = "circuitBreaker.enabled",value = "true"),                   //设置熔断
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold",value = "10"),
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds",value = "10000"),
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage",value = "60"),
     })
     @GetMapping("/getProductInfoList")
-    public String getProductInfoList(){
+    public String getProductInfoList(@RequestParam("number")Integer number){
         RestTemplate restTemplate = new RestTemplate();
+        if(number%2==0) return "success";
         return  restTemplate.postForObject("http://127.0.0.1:8082/product/listForOrder",
                 Arrays.asList("157875196366160022"),
                 String.class);
@@ -34,9 +39,3 @@ public class HystrixController {
         return "默认提示:太拥挤了,请稍后再试~~";
     }
 }
-
-//        throw new RuntimeException("发生异常了");
-//        if(服务太多,并发太高)
-/*private String fallback(){
-        return "太拥挤了,请稍后再试~~";
-    }*/
