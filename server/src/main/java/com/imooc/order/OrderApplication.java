c6508b4a1d156dcf268b4cfe609d7b9cb1245653
zhangchen <zhangchen0419@qq.com>
2019-07-02 18:51:45 +0800

    SpringCloud入门(七) 第四章 4.2 feign-hystrix的使用

--- server/src/main/java/com/imooc/order/OrderApplication.java
+++ server/src/main/java/com/imooc/order/OrderApplication.java
@@ -6,12 +6,14 @@ import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 /*@SpringBootApplication
 @EnableDiscoveryClient
 @EnableCircuitBreaker*/
 @EnableFeignClients(basePackages = "com.imooc.product.client")
 @SpringCloudApplication
+@ComponentScan(basePackages = "com.imooc")
 public class OrderApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OrderApplication.class, args);
